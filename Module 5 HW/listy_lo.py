58964b6b29facaf48954893fac3593770644d44e
johnngule4 <67490485+johnngule4@users.noreply.github.com>
2020-07-04 23:31:23 -0700

    Add files via upload

---
+++ Module 5 HW/listy_lo.py
@@ -0,0 +1,25 @@
+
+
+if __name__ == "__main__":
+
+    # Food List
+
+    # food = ['rice','beans']
+    # print(food)
+
+    # additional_food = ('broccoli')
+    # food.append(additional_food)
+    # print(food)
+
+    # more_food = (['bread','pizza'])
+    # food.extend(more_food)
+    # print(food)
+    # print(food[0:2])
+
+
+    # Breakfast List
+
+    breakfast = ("eggs, fruit, orange juice")
+
+    brofist = breakfast.split(',')
+    print(breakfast)
