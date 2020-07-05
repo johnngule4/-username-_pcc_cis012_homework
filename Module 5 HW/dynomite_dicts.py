5759661968621ca8b8ef4837d05775ceaeaa7736
johnngule4 <67490485+johnngule4@users.noreply.github.com>
2020-07-04 22:35:00 -0700

    Add files via upload

---
+++ Module 5 HW/dynomite_dicts.py
@@ -0,0 +1,15 @@
+
+
+
+if __name__ == "__main__":
+
+    pokedex = {
+        'Venosaur': ['Grass', 'Poison'],
+        'Charizard': ['Fire', 'Flying'],
+        'Blastoise': ['Water']
+    }
+    print(pokedex)
+
+    del pokedex['Blastoise']
+    print(pokedex)
+    
